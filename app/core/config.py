0ec37cfb891b823723bef4428bd56de87040f76d
Daniel Aureliano <daniel.aureliano@gmail.com>
2026-01-07 10:25:12 -0300

    chore(config): Altera default REDIS_URL para 127.0.0.1

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     # Redis
-    REDIS_URL: str = "redis://localhost:6379"
+    REDIS_URL: str = "redis://127.0.0.1:6379"
 
     # ANP
     ANP_BASE_URL: str = "https://www.gov.br/anp/pt-br/assuntos/precos-e-defesa-da-concorrencia/precos/arquivos-lpc"
@@ -16,14 +17,13 @@ class Settings(BaseSettings):
     GH_TOKEN: str | None = None
     GEMINI_API_KEY: str | None = None
     RENDER_DEPLOY_HOOK_URL: str | None = None
-    
+
     # Segurança da Aplicação
     REDIS_PASSWORD: str | None = None
 
     model_config = SettingsConfigDict(
-        env_file=".env", 
-        env_file_encoding="utf-8",
-        extra="ignore"
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
+
 
 settings = Settings()
